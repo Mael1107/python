9c622366d754e57d7649ebd4454bab1b6fc2dcb6
Ismael de Araujo <araujoismael0982@gmail.com>
2025-07-06 22:19:18 -0300

    conversos de real-dolar e tabuada-rudimentar

---
+++ d007/main.py
@@ -0,0 +1,8 @@
+#  Desafio 010 em CursoemVideo
+
+money = float(input("How much do you have in your wallet (R$) ? "))
+
+dollar =  money / 5.42
+
+print(f"With R${money:.2f} you can buy U${dollar:.2f}") 
+
