8eb8d626043a4f7e9ae555776cb285c80ab72612
Ismael de Araujo <araujoismael0982@gmail.com>
2025-07-07 23:38:19 -0300

    .

---
+++ d012/main.py
@@ -0,0 +1,10 @@
+# Desafio 015 em CursoemVideo
+
+d= int(input("How many days did you rent the car? \n"))
+
+km = float(input("How many miles you drove? \n"))
+
+pay = (d * 60.00) + (km * 0.15)
+
+print(f"The amount to be paid is R${pay:.2f}")
+
---
+++ d013/main.py
@@ -0,0 +1,7 @@
+# Desafio 016 em CursoemVideo
+
+x = float(input("Enter a value: "))
+
+y = round(x)
+
+print(f"The arredondo value is {y}")
