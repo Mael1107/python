ca02abd339dd22c35318c764f746dc54ff28f42c
Ismael de Araujo <araujoismael0982@gmail.com>
2025-07-22 23:32:58 -0300

    .

--- d026/main.py
+++ d026/main.py
@@ -2,7 +2,7 @@
 
 velocity= float(input("How many km/h were you driving? "))
 
-if velocity> 80:
+if velocity > 80:
     fine = 7 * (velocity - 80)
     print(f"You have exceeded the maximum speed of 80km/h and were fined in R${fine:.2f}")
 else:
