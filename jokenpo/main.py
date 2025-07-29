515f0120e1036d12d39a602e774a6e71137a1a31
Ismael de Araujo <araujoismael0982@gmail.com>
2025-07-29 12:14:30 -0300

    Update main.py

--- jokenpo/main.py
+++ jokenpo/main.py
@@ -1,7 +1,41 @@
 # Desafio 045 em CursoemVideo
 from random import choice
+import os
 
 jokenpo = ["Rock", "Paper", "Scissors"]
 pc_choice = choice(jokenpo)
 
-player_choice = int(input(""))
+player_choice = int(input(
+    """
+    Your options: 
+    [0] - Rock
+    [1] - Paper
+    [2] = Scissors
+
+    What is your move? 
+    """))
+
+os.system("cls" if os.name == "nt" else "clear")
+
+print(f"I chose {pc_choice}")
+
+if pc_choice == "Rock" and player_choice == 1:
+    print("You \033[1;32mWON\033[m!")
+elif pc_choice == "Rock" and player_choice == 0:
+    print("We \0331;33[mDRAW\033[m!")
+elif pc_choice == "Rock" and player_choice == 2:
+    print("You \033[1;31mLOST\033[m!")
+elif pc_choice == "Paper" and player_choice == 2:
+    print("You \033[1;32mWON\033[m!")
+elif pc_choice == "Paper" and player_choice == 1:
+    print("We \0331;33[mDRAW\033[m!")
+elif pc_choice == "Paper" and player_choice == 0:
+    print("You \033[1;31mLOST\033[m!")
+elif pc_choice == "Scissors" and player_choice == 0:
+    print("You \033[1;32mWON\033[m!")
+elif pc_choice == "Scissors" and player_choice == 2:
+    print("We \0331;33[mDRAW\033[m!")
+elif pc_choice == "Scissors" and player_choice == 1:
+     print("You \033[1;31mLOST\033[m!")
+else:
+    print("Invalid!")
