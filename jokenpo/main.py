29769ea32a5435394bf8eab2c48510ac5a8376c7
Ismael de Araujo <araujoismael0982@gmail.com>
2025-07-29 13:05:37 -0300

    .

--- jokenpo/main.py
+++ jokenpo/main.py
@@ -10,7 +10,7 @@ player_choice = int(input(
     Your options: 
     [0] - Rock
     [1] - Paper
-    [2] = Scissors
+    [2] - Scissors
 
     What is your move? 
     """))
@@ -22,20 +22,20 @@ print(f"I chose {pc_choice}")
 if pc_choice == "Rock" and player_choice == 1:
     print("You \033[1;32mWON\033[m!")
 elif pc_choice == "Rock" and player_choice == 0:
-    print("We \0331;33[mDRAW\033[m!")
+    print("We \033[1;33[mDRAW\033[m!")
 elif pc_choice == "Rock" and player_choice == 2:
     print("You \033[1;31mLOST\033[m!")
 elif pc_choice == "Paper" and player_choice == 2:
     print("You \033[1;32mWON\033[m!")
 elif pc_choice == "Paper" and player_choice == 1:
-    print("We \0331;33[mDRAW\033[m!")
+    print("We \033[1;33[mDRAW\033[m!")
 elif pc_choice == "Paper" and player_choice == 0:
     print("You \033[1;31mLOST\033[m!")
 elif pc_choice == "Scissors" and player_choice == 0:
     print("You \033[1;32mWON\033[m!")
 elif pc_choice == "Scissors" and player_choice == 2:
-    print("We \0331;33[mDRAW\033[m!")
+    print("We \033[1;33[mDRAW\033[m!")
 elif pc_choice == "Scissors" and player_choice == 1:
-     print("You \033[1;31mLOST\033[m!")
+    print("You \033[1;31mLOST\033[m!")
 else:
     print("Invalid!")
