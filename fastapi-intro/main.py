f5a5210d5ce4aa1889ab76ae2ff310b61a95df53
mael <araujoismael0982@gmail.com>
2026-09-01 11:04:09 -0300

    feat: CRUD fastapi completed

--- fastapi-intro/main.py
+++ fastapi-intro/main.py
@@ -21,6 +21,12 @@ def create_products(product: Product):
 def list_products():
     return products_db
 
+@app.get("/products/{product_id}")
+def show_product(product_id: int):
+    for product in products_db:
+        if product.id == product_id:
+            return product
+
 @app.put("/products/{product_id}")
 def edit_product(product_id: int, updated_product: Product):
     for product in products_db:
@@ -30,5 +36,11 @@ def edit_product(product_id: int, updated_product: Product):
             product.available = updated_product.available
             return product
 
+@app.delete("/products/{product_id}")
+def delete_product(product_id: int):
+    for product in products_db:
+        if product.id == product_id:
+            products_db.remove(product)
+            return {"detail": "product removed"}
 
  
